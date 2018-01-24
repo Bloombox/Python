295d2c7acf67d7017cc8893c06d02fa92371b713
Sam Gammon <sam@momentum.io>
2018-01-23 18:10:38 -0800

    Fix spacing issue surfaced by CodeClimate

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from pip.req import parse_requirements
 setup(
   name="bloombox",
   version="0.0.1",
-  package_dir={'':'src'},
+  package_dir={'': 'src'},
   packages=find_packages('src', exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
   namespace_packages=['bloombox'],
   install_requires=[
